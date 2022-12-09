c3af6f5960f258eafa957680c384957f7be2bcaf
Emerson Carvalho <emersoncarvalho@hotmail.com.br>
2022-12-09 20:47:53 -0300

    Exercicio12

---
+++ Desafios/exercicio11.php
@@ -0,0 +1,20 @@
+<table border=2>
+    <thead>
+        <tr>
+            <th>Contatos</th>
+        </tr>   
+    </thead>
+    <tbody>
+        <?php
+        $nomearquivo = "contatos.txt";
+        $arquivo = fopen($nomearquivo, "r");
+        while(!feof($arquivo)){
+            $linha = fgets($arquivo, 100);
+            echo "<tr>";
+            echo "<td>$linha</td>";
+            echo "</tr>";
+        }
+        fclose($arquivo);
+        ?>
+    </tbody>
+</table>
